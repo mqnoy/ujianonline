b7b81aab375b76b0d1571653c8ba224be09f604a
mqnoy <1677058+mqnoy@users.noreply.github.com>
2019-05-16 15:23:35 +0700

    remove validation function js

--- form_soal.php
+++ form_soal.php
@@ -12,7 +12,7 @@ include "./includes/Soal_list.php";
 //panggil function soal list
 $soal = tampil_soal();
 $tampil_pilihanGanda = function ($no_soal=1) use ($soal){
-    echo "<div class=\"tab\"> ";
+    echo "<div class=\"div_pilganda\" id=\"pil_ganda\">";
     echo "<h4>Soal <span id=\"posisi_soal\">$no_soal</span></h4>";
     echo $soal['pilganda_soal_'.$no_soal]['soal'];
     foreach ($soal['pilganda_soal_'.$no_soal]['pilihan_ganda'] as $key => $value) {
@@ -30,6 +30,7 @@ if (isset($_POST['submit_soal'])) {
   header("Location: ./jawaban.php");
 }
 ?>
+
 <div id="regForm" >
 <form method="post" id="form_soal" action="<?php echo $_SERVER['PHP_SELF'];?>">
   <?php 
@@ -38,98 +39,82 @@ if (isset($_POST['submit_soal'])) {
     $tampil_pilihanGanda($i);
   }
   ?>
-  <div style="overflow:auto;">
+  <div class="footer_form_soal">
     <div style="float:right;">
       <button type="button" id="prevBtn" onclick="nextPrev(-1)">Previous</button>
       <button type="button" id="nextBtn" onclick="nextPrev(1)">Next</button>
     </div>
-  </div>
-  <!-- Circles which indicates the steps of the form: -->
-  <div style="text-align:center;margin-top:40px;">
-  <?php
-    for ($j=1; $j <= sizeof($soal); $j++) { 
-      # code...
-      echo "<span class=\"step\"></span>";
-    }
-  ?>
+    <!-- Circles which indicates the steps of the form: -->
+    <div class="title_regForm">
+      <?php
+        for ($j=1; $j <= sizeof($soal); $j++) { 
+          # code...
+          echo "<span class=\"step\"></span>";
+        }
+      ?>
+    </div>
   </div>
 </form>
 </div>
+<div id="div_jawaban">
+
+
+</div>
+
 <script>
-var currentTab = 0; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the current tab
+  //https://www.w3schools.com/howto/howto_js_form_steps.asp?
+  var currentTab = 0; // Current tab is set to be the first tab (0)
+  showTab(currentTab); // Display the current tab
 
-function showTab(n) {
-  // This function will display the specified tab of the form...
-  var x = document.getElementsByClassName("tab");
-  x[n].style.display = "block";
-  //... and fix the Previous/Next buttons:
-  if (n == 0) {
-    document.getElementById("prevBtn").style.display = "none";
-  } else {
-    document.getElementById("prevBtn").style.display = "inline";
-  }
-  if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").innerHTML = "Submit";    
-  } else {
-    document.getElementById("nextBtn").innerHTML = "Next";
-  }
-  //... and run a function that will display the correct step indicator:
-  fixStepIndicator(n)
-}
-
-function nextPrev(n) {
-  // This function will figure out which tab to display
-  var x = document.getElementsByClassName("tab");
-  // Exit the function if any field in the current tab is invalid:
-  if (n == 1 && !validateForm()) return false;
-  // Hide the current tab:
-  x[currentTab].style.display = "none";
-  // Increase or decrease the current tab by 1:
-  currentTab = currentTab + n;
-  // if you have reached the end of the form...
-  var submited_form = false;
-  if (currentTab >= x.length) {
-    var tombol_submit = document.getElementById("nextBtn");
-    var att = document.createAttribute("name");
-    att.value = "submit_soal";
-    tombol_submit.setAttribute("type", "submit"); 
-
-    // var tombol_submit = document.getElementById("nextBtn");
-    tombol_submit.setAttribute("type", "submit"); 
-    tombol_submit.setAttributeNode(att);
-    submited_form= true;
-  }
-  if (submited_form) {
-    // ... the form gets submitted:
-    document.getElementById("form_soal").submit();
-      
-    return false;
-  }
-  // Otherwise, display the correct tab:
-  showTab(currentTab);
-}
-
-function validateForm() {
-  // This function deals with validation of the form fields
-  var x, y, i, valid = true;
-  x = document.getElementsByClassName("tab");
-  y = x[currentTab].getElementsByTagName("input");
-  // A loop that checks every input field in the current tab:
-  for (i = 0; i < y.length; i++) {
-    // If a field is empty...
-    if (y[i].value == "") {
-      // add an "invalid" class to the field:
-      y[i].className += " invalid";
-      // and set the current valid status to false
-      valid = false;
+  function showTab(n) {
+    // This function will display the specified tab of the form...
+    var div_pilganda = document.getElementsByClassName("div_pilganda");
+    div_pilganda[n].style.display = "block";
+    //... and fix the Previous/Next buttons:
+    if (n == 0) {
+      document.getElementById("prevBtn").style.display = "none";
+    } else {
+      document.getElementById("prevBtn").style.display = "inline";
     }
+    if (n == (div_pilganda.length - 1)) {
+      document.getElementById("nextBtn").innerHTML = "Submit";    
+    } else {
+      document.getElementById("nextBtn").innerHTML = "Next";
+    }
+    //... and run a function that will display the correct step indicator:
+    fixStepIndicator(n)
   }
-  // If the valid status is true, mark the step as finished and valid:
-  if (valid) {
-    document.getElementsByClassName("step")[currentTab].className += " finish";
-  }
-  return valid; // return the valid status
+
+  function nextPrev(n) {
+    // This function will figure out which tab to display
+    var div_pilganda = document.getElementsByClassName("div_pilganda");
+    
+    // Hide the current tab:
+    div_pilganda[currentTab].style.display = "none";
+    // Increase or decrease the current tab by 1:
+    currentTab = currentTab + n;
+
+    // if you have reached the end of the form...
+    var submited_form = false;
+    if (currentTab >= div_pilganda.length) {
+      var tombol_submit = document.getElementById("nextBtn");
+      var att = document.createAttribute("name");
+      att.value = "submit_soal";
+      tombol_submit.setAttribute("type", "submit"); 
+
+      // var tombol_submit = document.getElementById("nextBtn");
+      tombol_submit.setAttribute("type", "submit"); 
+      tombol_submit.setAttributeNode(att);
+      // submited_form= true;
+    }
+    if (submited_form) {
+      // ... the form gets submitted:
+      document.getElementById("form_soal").submit();
+        
+      return false;
+    }
+    // Otherwise, display the correct tab:
+    showTab(currentTab);
 }
 
 function fixStepIndicator(n) {
