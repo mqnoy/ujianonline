80dc24d57abf7f0e60cd9b010650010dedbdf577
mqnoy <1677058+mqnoy@users.noreply.github.com>
2019-05-28 16:56:14 +0700

    add list soal at dashboard siswa

--- siswa/siswa_list_soal.php
+++ siswa/siswa_list_soal.php
@@ -1,21 +1,38 @@
-<?php if($_SESSION['is_siswa'] == true && isset($_GET['halaman']) && $_GET['halaman'] === "list_soal"){ ?>
-<div class="col-md-3 col-sm-6 col-xs-12">
-          <div class="info-box bg-aqua">
-            <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
+<?php 
+if($_SESSION['is_siswa'] == true && isset($_GET['halaman']) && $_GET['halaman'] === "list_soal"){ 
 
-            <div class="info-box-content">
-              <span class="info-box-text">Bookmarks</span>
-              <span class="info-box-number">41,410</span>
+$data_matpel_siswa = $models->select_matpel_kelas();  
+$total_soal=[];
+foreach ($data_matpel_siswa as $value) {
+  # code...
+  $i=0;
+  $total_soal = $models->select_count("tabel_soal","matpel_id","=",$value['id_matpel']);
+  // var_dump($total_soal);
+  ?>
+  <div class="col-md-3 col-sm-6 col-xs-12">
+            <div class="info-box bg-aqua">
+              <span class="info-box-icon"><i class="fa fa-bookmark-o"></i></span>
 
-              <div class="progress">
-                <div class="progress-bar" style="width: 70%"></div>
+              <div class="info-box-content">
+                <span class="info-box-text"><?php echo $value['nama_matpel'];?></span>
+                <span class="info-box-number"><?php echo $total_soal[$i]['total_data'];?></span>
+
+                <div class="progress">
+                  <div class="progress-bar" style="width: 70%"></div>
+                </div>
+                    <span class="progress-description">
+                      <?php echo $value['txt_kelas'];?>
+                    </span>
               </div>
-                  <span class="progress-description">
-                    70% Increase in 30 Days
-                  </span>
+              <!-- /.info-box-content -->
             </div>
-            <!-- /.info-box-content -->
+            <!-- /.info-box -->
           </div>
-          <!-- /.info-box -->
-        </div>
-<?php }  ?>
+    <?php
+    // $i++;
+  }/** end foreach  */
+  ?>
+
+<?php 
+}  
+?>
