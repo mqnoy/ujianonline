ee6619091cfcec06b6be7667e2d0b6e1e3b59c36
mqnoy <1677058+mqnoy@users.noreply.github.com>
2019-05-28 08:10:20 +0700

    add query select

--- admin/form_master_kunci_jawaban.php
+++ admin/form_master_kunci_jawaban.php
@@ -1,29 +1,28 @@
 <?php
 $list_matpel = $models->select_matpel();
 $list_kelas = $models->select_kelas();
-$ck_editor = false;
 ?>
 <div class="modal fade" id="modal-pilihan-ganda">
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span></button>
-                <h4 class="modal-title">Default Modal</h4>
-              </div>
-              <div class="modal-body">
-                <p>One fine body&hellip;</p>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save changes</button>
-              </div>
-            </div>
-            <!-- /.modal-content -->
-          </div>
-          <!-- /.modal-dialog -->
-        </div>
-        <!-- /.modal -->
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title">Default Modal</h4>
+			</div>
+			<div class="modal-body">
+				<p>One fine body&hellip;</p>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-primary">Save changes</button>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
 <div class="col-md-12">
 	<div class="box box-primary">
 		<div class="box-header with-border">
@@ -31,17 +30,36 @@ $ck_editor = false;
 		</div>
 		<!-- /.box-header -->
 		<div class="box-body">
-			<table class="table table-bordered">
+			<div class="form-group col-xs-3">
+				<label>Kelas</label>
+				<select id="cb_kelas" class="form-control" name="nm_kelas">
+					<option value="0">pilih kelas</option>
+					<?php
+					foreach ($list_kelas as $value) {
+						# code...
+						echo "<option value=\"" . $value['id_kelas'] . "\">" . $value['txt_kelas'] . "</option>";
+					}
+					?>
+				</select>
+			</div>
+			<div class="form-group col-xs-3">
+			<label>Mata pelajaran</label>
+			<select id="cb_matpel" class="form-control name="nm_matpel">
+				</select>
+			</div>
+
+			<table class="table table-bordered" id="tabel_kunci_jawaban">
 				<tbody>
 					<tr>
 						<th style="width: 10px">#</th>
-						<th>Soal</th>
-						<th>Kelas</th>
-						<th>Matpel</th>
-						<th>Kunci jawaban</th>
-						<th>Tetapkan jawaban</th>
+						<th>nomor Soal</th>
+						<th>soal</th>
+						<th>jawaban</th>
+						<th>mata pelajaran</th>
+						<th>kelas</th>
+						<th>aksi</th>
 					</tr>
-					<tr>
+					<!-- <tr>
 						<td>1.</td>
 						<td>Update software</td>
 						<td>
@@ -57,7 +75,7 @@ $ck_editor = false;
 								</a>
 							</div>
 						</td>
-					</tr>
+					</tr> -->
 				</tbody>
 			</table>
 		</div>
