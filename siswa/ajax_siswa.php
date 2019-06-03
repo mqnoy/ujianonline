d89c76f522c07590da4d6332cd3c43e555952f40
mqnoy <qnoy@imzazmi.com>
2019-06-03 22:59:22 +0700

    remove unused function and move to ajax.php

---
+++ siswa/ajax_siswa.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+if (isset($_POST['login_siswa'])) {
+    # code...
+    // $_SESSION['nis_siswa'];
+    if (empty($_POST['siswa_nis']) || empty($_POST['siswa_nama'])) {
+        # code...
+        header('Location: ../siswa.php?login_err=101');
+    } else {
+        $_SESSION['ses_nis_siswa'] = $_POST['siswa_nis'];
+        $_SESSION['ses_nama_siswa'] = $_POST['siswa_nama'];
+        $_SESSION['is_logged'] = true;
+        $_SESSION['is_admin'] = false;
+        $_SESSION['is_siswa'] = true;
+        $_SESSION['halaman_aktif'] = "Dashboard";
+        header('Location: ../dashboard.php');
+    }
+}
+
