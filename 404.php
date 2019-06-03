d071fb09dd9bfa85992c6c5a1bea7ab4c7d6ded8
mqnoy <qnoy@imzazmi.com>
2019-06-03 22:58:36 +0700

    add halaman tidak  ditemukan

---
+++ 404.php
@@ -0,0 +1,22 @@
+<div class="error-page">
+        <h2 class="headline text-yellow"> 404</h2>
+
+        <div class="error-content">
+          <h3><i class="fa fa-warning text-yellow"></i> Oops! Halaman tidak di temukan.</h3>
+
+          <p>
+            Kami tidak dapat menemukan halaman yang kamu minta silahkan <a href="dashboard.php">kembali ke halaman utama</a> </p>
+
+          <!-- <form class="search-form">
+            <div class="input-group">
+              <input type="text" name="search" class="form-control" placeholder="Search">
+
+              <div class="input-group-btn">
+                <button type="submit" name="submit" class="btn btn-warning btn-flat"><i class="fa fa-search"></i>
+                </button>
+              </div>
+            </div>
+          </form> -->
+        </div>
+        <!-- /.error-content -->
+      </div>
