13e62815b6cea3969c1f9e659ba4944e52a104c6
mqnoy <qnoy@imzazmi.com>
2019-06-04 03:07:18 +0700

    put file code ajax_siswa.php

--- siswa.php
+++ siswa.php
@@ -2,21 +2,69 @@
 session_start();
 if (isset($_GET['action']) && $_GET['action']=="logout") {
     # code...
-    session_start();
     session_destroy();
     session_unset();
     header('Location: ./siswa.php?msg=logout');
-  }
+}
+include "./templates/header.php";
+require('./includes/Query.php');
+
+if (isset($_POST['login_siswa'])) {
+    # code...
+    // $_SESSION['nis_siswa'];
+    if (empty($_POST['siswa_nis']) || empty($_POST['siswa_nama'])) {
+        # code...
+        header('Location: siswa.php?login_err=101');
+    } else {
+        $_SESSION['ses_nis_siswa'] = $_POST['siswa_nis'];
+        $_SESSION['ses_nama_siswa'] = $_POST['siswa_nama'];
+        $_SESSION['is_logged'] = false;
+        $_SESSION['is_admin'] = false;
+        $_SESSION['is_siswa'] = true;
+
+        $nis_siswa  = $_SESSION['ses_nis_siswa'];
+        $nama_siswa = $_SESSION['ses_nama_siswa'];
+        
+        $token_siswa= generate_tokenSiswa($nis_siswa);//params string
+
+        $insData = array (
+            'siswa_nis' => $nis_siswa,
+            'siswa_nama' => $nama_siswa ,
+            'siswa_kelas_id' => 0 ,
+            'token_siswa' => $token_siswa ,
+            'tgl_terdaftar' => tgl_waktu_skrg()
+        );
+        $select_siswa = $models->select_from("master_siswa","siswa_nis","=",$nis_siswa);
+          if ($select_siswa == null) {
+            # code...
+          $insert_siswa = $models->insert_into("master_siswa",$insData);
+          var_dump($insert_siswa);
+          if ($insert_siswa) {
+            # code...
+            $_SESSION['is_logged'] = true;
+            header('Location: dashboard.php');
+          }else{
+            $_SESSION['is_logged'] = false;
+            header('Location: siswa.php?login_err=201');
+          }
+        }else{
+          foreach ($select_siswa as $siswa) {
+            # code...
+            $_SESSION['ses_nis_siswa']  =$siswa['siswa_nis'];
+            $_SESSION['ses_nama_siswa'] =$siswa['siswa_nama'];
+          }
+          $_SESSION['is_logged'] = true;
+          header('Location: dashboard.php');
+        }
+
+    }
+}
 if (isset($_SESSION['ses_nis_siswa']) && isset($_SESSION['ses_nama_siswa']) && $_SESSION['is_logged']) {
     # code...
-    header('Location: ./dashboard.php');
+    // header('Location: ./dashboard.php');
 }
+
 ?>
-<!DOCTYPE html>
-<html>
-  <?php
-    include "./templates/header.php";
-  ?>  
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
@@ -26,7 +74,7 @@ if (isset($_SESSION['ses_nis_siswa']) && isset($_SESSION['ses_nama_siswa']) && $
   <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
 
-    <form action="./siswa/ajax_siswa.php" method="post">
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
       <div class="form-group has-feedback">
         <input type="text" class="form-control" name="siswa_nis" placeholder="nis">
         <!-- <span class="glyphicon glyphicon-envelope form-control-feedback"></span> -->
