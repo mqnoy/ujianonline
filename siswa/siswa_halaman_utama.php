f447517780293582f7f1c5d51c57ccd6300ef174
mqnoy <qnoy@imzazmi.com>
2019-06-04 03:08:18 +0700

    add field token and event click for copy

--- siswa/siswa_halaman_utama.php
+++ siswa/siswa_halaman_utama.php
@@ -2,6 +2,12 @@
   if ($_SESSION['is_admin'] == false && $_SESSION['is_siswa'] == true && !isset($_GET['halaman'])) {
     $nama_siswa = $_SESSION['ses_nama_siswa'];
     $nis_siswa = $_SESSION['ses_nis_siswa'];
+    $token_siswa="";
+    $select_siswa = $models->select_from("master_siswa","siswa_nis","=",$nis_siswa);
+    foreach ($select_siswa as $value) {
+      # code...
+      $token_siswa = $value['token_siswa'];
+    }
     if(!isset($_SESSION['ses_id_matpel'])){
     ?>
     <!--start dashboard siswa-->
@@ -10,13 +16,19 @@
           <!-- Profile Image -->
           <div class="box box-primary">
             <div class="box-body box-profile">
-              <img class="profile-user-img img-responsive img-circle" src="<?php echo base_url('assets/img/user4-128x128.jpg'); ?>" alt="User profile picture">
+              <img class="profile-user-img img-responsive img-circle" src="<?php echo base_url('assets/img/avatar5.png'); ?>" alt="User profile picture">
               <h3 class="profile-username text-center"><?php echo $nama_siswa; ?></h3>
               <p class="text-muted text-center"><?php echo $nis_siswa; ?></p>
               <div class="form-group">
+                <label>token</label>
+                <input type="text" class="form-control" value="<?php echo $token_siswa;?>" id="txt_token_siswa"/>
+                <p class="help-block">klik kolom token untuk copy token</p>
+              </div>
+              <div class="form-group">
                 <label>Kelas</label>
                 <select class="form-control" name="name_kelas_siswa" id="cb_kelas_siswa">
-                </select></div>
+                </select>
+              </div>
             </div>
             <!-- /.box-body -->
           </div>
