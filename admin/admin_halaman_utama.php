5344f58012e655e9ca185247d86a85c974b35035
mqnoy <qnoy@imzazmi.com>
2019-06-04 18:42:08 +0700

    i dont know

--- admin/admin_halaman_utama.php
+++ admin/admin_halaman_utama.php
@@ -2,57 +2,16 @@
         <div class="col-md-12">
   <div class="box box-primary">
     <div class="box-header with-border">
-      <h3 class="box-title">Input pilihan ganda</h3>
+      <h3 class="box-title">Petunjuk penggunaan</h3>
     </div>
     <!-- /.box-header -->
-    <!-- form start -->
-    <form role="form" id="form_pilihanganda">
-    <input type="hidden" name="fr_post_pilihanganda" value="post_pilganda"/>
-    <input type="hidden" name="soalid"/>
       <div class="box-body">
-      <div class="form-group">
-            <label>Kelas</label>
-            <select class="form-control" name="matpel_kelas" id="cb_kelas">
-            </select>
-        </div>
-        <div class="form-group">
-          <label>Mata pelajaran</label>
-          <select id="cb_matpel" class="form-control">
-          </select>
-        </div>
-        <div class="input-group form-group col-md-2">
-          <label>Nomor pertanyaan</label>
-          <select id="cb_nomor_soal" class="form-control" name="nomor_soal">
-          </select>
-        </div>
-        <div class="form-group" id="group_pertanyaan" style="display:none;">
-          <label>Pertanyaan</label>
-          <textarea id="editor1" name="txt_pertanyaan" rows="10" cols="80" disabled></textarea>
-        </div>
-
-        <div class="input-group col-md-3" id="group_pg_soal" style="display:none;">
-          <div class="input-group margin">
-            <span class="input-group-addon">A</span>
-            <input type="text" name="pg_soal[]" class="form-control" placeholder="pilihan ganda A">
-          </div>
-          <div class="input-group margin">
-            <span class="input-group-addon">B</span>
-            <input type="text" name="pg_soal[]" class="form-control" placeholder="pilihan ganda B">
-          </div>
-          <div class="input-group margin">
-            <span class="input-group-addon">C</span>
-            <input type="text" name="pg_soal[]" class="form-control" placeholder="pilihan ganda C">
-          </div>
-          <div class="input-group margin">
-            <span class="input-group-addon">D</span>
-            <input type="text" name="pg_soal[]" class="form-control" placeholder="pilihan ganda D">
-          </div>
-        </div>
+      
       </div>
       <!-- /.box-body -->
 
       <div class="box-footer">
-        <button type="submit" class="btn btn-primary pull-right" id="submit_pilganda">Simpan</button>
+        <button type="submit" class="btn btn-primary pull-right" id="">Simpan</button>
       </div>
     </form>
   </div>
