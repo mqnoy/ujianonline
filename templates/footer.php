2e92f2a550915d57e798c1083aaf3137e122179c
mqnoy <qnoy@imzazmi.com>
2019-06-06 18:10:13 +0700

    re

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,4 @@
-<!-- jQuery 3 -->
+<!-- jQuery 3 --->
 <script src="./assets/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
 <script src="./assets/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
