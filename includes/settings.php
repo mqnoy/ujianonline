b9b3a59aa8a6ab0e3f16c82c462e00691f2e3eaf
mqnoy <1677058+mqnoy@users.noreply.github.com>
2019-06-10 08:48:26 +0700

    add gitignore

--- includes/settings.php
+++ includes/settings.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Jakarta");
 // database settings
 define("DB_HOST","localhost");
 define("DB_USERNAME","root");
-define("DB_USER_PASSWORD","123456");
+define("DB_USER_PASSWORD","");
 define("DB_NAME","db_ujianonline");
 
 include_once "app.php";
